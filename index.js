f6aa52202284a68a8deca9205f3dfa73b1540809
Пётр Румянцев <dogboxer64529000@gmail.com>
2022-12-02 23:46:30 +0300

    Сверстать меня при скролле

---
+++ index.js
@@ -0,0 +1,11 @@
+const scrollElement = document.querySelector('.scroll');
+document.addEventListener('scroll', () => {
+  let distance = window.scrollY
+  if (distance >= 450) {
+    scrollElement.setAttribute('style', 'display:flex')
+
+  }
+  else {
+    scrollElement.removeAttribute('style')
+  }
+})
